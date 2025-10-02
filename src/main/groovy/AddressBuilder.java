16132824265b0e767d52a551a8a5fd72b40bc3c0
jesicamatul <evelinmtlj@gmail.com>
2025-10-01 20:36:00 -0400

    making if statements to check is same address for both

--- src/main/groovy/AddressBuilder.java
+++ src/main/groovy/AddressBuilder.java
@@ -5,28 +5,51 @@ public class AddressBuilder {
         public static void main(String[] args) { // strings
             // Adress builder
 
-            Scanner scanner = new Scanner(System.in);
+           Scanner scanner = new Scanner(System.in);
             StringBuilder address = new StringBuilder();
             String fullName = enterDaa("Enter full name: ");
             String billingStreet = enterDaa("Enter billing street: ");
             String billingState = enterDaa("Enter billing state: ");
+            String billingCity = enterDaa("Enter billing city: ");
             String billingZip = enterDaa("Enter zip code: ");
-            String shippingStreet = enterDaa("Enter shipping street: ");
-            String shipCity = enterDaa("Enter shipping state: ");
-            String shipZip = enterDaa("Enter shipping zip code: ");
+            // ask if shipping address is also the same
+// create a boolean statement for same address
+            System.out.println("Is shipping address the same as billing address? Y or N: ");
+            String answer = scanner.nextLine().trim().toLowerCase();
+            boolean response = answer.equals("y");
 
-            String sameAdress = enterDaa("Is shipping address the same as billing adress? ");
 
-            address.append
+            String shippingStreet = " ";
+           String  shippingCity = " ";
+           String  shippingState = " ";
+           String  shippingZip = " ";
+
+// ELSE IS IF BOTH ADDRESSES ARE NOT THE SAME
+           if (response) {
+    billingStreet = shippingStreet;
+    billingState = shippingState;
+    billingCity = shippingCity;
+    billingZip = shippingZip;
+           } else {
+               shippingStreet = enterDaa("Enter shipping street: ");
+               shippingState = enterDaa("Enter shipping state: ");
+               shippingCity = enterDaa("Enter shipping city:");
+               shippingZip = enterDaa("Enter shipping zip code: ");
+           }
+
+
 
 
 
 
         } // create private to ask user information
-        private static String enterDaa (String info)  {
-            System.out.print(info + " ");
-            return Scanner.nextLine().trim();
+        private static String enterDaa (String info )  {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print(info);
+            return scanner.nextLine();
         }
+
+
     }
 
 
